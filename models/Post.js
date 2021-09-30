5838b0bed8202ae4dbb48e140e4b0c97bd20488d
SHARP <ssharp9999@gmail.com>
2021-09-29 20:22:51 -0700

    model def

---
+++ models/Post.js
@@ -0,0 +1,12 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../db')
+
+class Post extends Model { }
+
+// initialize Post table/columns
+Post.init({
+  title: DataTypes.STRING,
+  body: DataTypes.STRING
+}, { sequelize, modelName: 'post' })
+
+module.exports = Post
