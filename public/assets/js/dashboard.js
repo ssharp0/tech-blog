61a861e8358ff6bdc38ae4f2888a4929ac3ed07e
SHARP <ssharp9999@gmail.com>
2021-10-05 21:14:53 -0700

    updated requests and toggle

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -98,9 +98,6 @@ axios.get('/api/users/posts', {
   // catch error
   .catch(err => console.error(err))
 
-
-
-
 // get element createPost and when clicked
 document.getElementById('createPost').addEventListener('click', event => {
   event.preventDefault()
@@ -158,13 +155,201 @@ document.getElementById('createPost').addEventListener('click', event => {
     .catch(err => console.error(err))
 })
 
+// button to open modal
+let btn = document.querySelectorAll("button.modal-button");
+// get all modals
+let modals = document.querySelectorAll('.modal');
+// span element closes the modal (x)
+let spans = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+for (let i = 0; i < btn.length; i++) {
+  btn[i].onclick = function (event) {
+    event.preventDefault();
+    modal = document.querySelector(event.target.getAttribute("href"));
+    modal.style.display = "block";
+  }
+}
+
+// When the user clicks on (x), close the modal
+for (let i = 0; i < spans.length; i++) {
+  spans[i].onclick = function () {
+    for (let index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+    }
+  }
+}
+
+// any clicks anywhere outside of the modal to close it
+window.onclick = function (event) {
+  // if the event target class list contains modal
+  if (event.target.classList.contains('modal')) {
+    for (let index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+    }
+  }
+}
+
+// function to toggle the elements to hide/unhide for edit and view section
+function toggle(elements, specifiedDisplay) {
+  let element, index;
+
+  // check elements length
+  elements = elements.length ? elements : [elements];
+  for (index = 0; index < elements.length; index++) {
+    element = elements[index];
+
+    // if the element is hidden
+    if (isElementHidden(element)) {
+      element.style.display = '';
+
+      // If the element is still hidden after removing the inline display
+      if (isElementHidden(element)) {
+        element.style.display = specifiedDisplay || 'block';
+      }
+    } else {
+      element.style.display = 'none';
+    }
+  }
+  function isElementHidden(element) {
+    return window.getComputedStyle(element, null).getPropertyValue('display') === 'none';
+  }
+}
+
+// global event listener for edit post button
+document.addEventListener('click', event => {
+  // if the event target class contains editToggle
+  if (event.target.classList.contains('viewToggle')) {
+    // toggle to show the edit post section
+    toggle(document.querySelectorAll('.viewEditPost'))
+    // get the post data by post id with auth
+    axios.get(`/api/post/${event.target.dataset.id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+      // then set the post data to the edit fields for user to edit
+      .then(({ data: post }) => {
+
+        console.log(post);
+
+        // set the data to the html body so user can view the post details
+        document.getElementById('viewEditTitle').value = post.title
+        document.getElementById('viewEditBody').value = post.body
+        document.getElementById('postViewEditFormId').value = post.id
+
+        // set readonly to true so fields can not be edited
+        document.getElementById("viewEditTitle").readOnly = true;
+        document.getElementById("viewEditBody").readOnly = true;
+
+        // hide the update and cancel button (only for edit posts)
+        document.getElementById('updateBtn').style.display = 'none'
+        // document.getElementById('cancelBtn').style.display = 'none'
+
+        // set the header inner thml to view post
+        document.getElementById('viewEditHeader').innerHTML = 'View Post'
+      })
+      // catch error
+      .catch(err => console.log(err))
+  }
+})
+
+// global event listener for edit post button
+document.addEventListener('click', event => {
+  // if the event target class contains editToggle
+  if (event.target.classList.contains('editToggle')) {
+    // toggle to show the edit post section
+    toggle(document.querySelectorAll('.viewEditPost'))
+    // get the post data by post id with auth
+    axios.get(`/api/post/${event.target.dataset.id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+      // then set the post data to the edit fields for user to edit
+      .then(({ data: post }) => {
+
+        // set the data to the html body so user can edit the post details
+        document.getElementById('viewEditTitle').value = post.title
+        document.getElementById('viewEditBody').value = post.body
+        document.getElementById('postViewEditFormId').value = post.id
+
+        // set readonly to false so fields can be edited
+        document.getElementById("viewEditTitle").readOnly = false;
+        document.getElementById("viewEditBody").readOnly = false;
+
+        // show the update button and change the text to show Edit Post
+        document.getElementById('updateBtn').style.display = 'in-line'
+        document.getElementById('viewEditHeader').innerHTML = 'Edit Post'
+      })
+      // catch error
+      .catch(err => console.log(err))
+  }
+})
+
+// global event listener when clicked
+document.addEventListener('click', event => {
+  // if the target class contains updatePostBtn
+  if (event.target.classList.contains('updatePostBtn')) {
+    // console.log('update btn clicked');
+    // set id to the post id
+    id = document.getElementById('postViewEditFormId').value
+    // put (edit) the post based on post id with values user updates in fields with auth
+    axios.put(`/api/posts/${id}`,
+      {
+        title: document.getElementById('viewEditTitle').value,
+        body: document.getElementById('viewEditBody').value
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      })
+      // then refresh the page
+      .then(() => {
+        alert('post updated')
+        location.reload()
+      })
+      // catch error
+      .catch(err => console.log(err))
+  }
+})
+
 // event listener when cancel button is clicked to refresh page with no other action
 document.getElementById('cancelBtn').addEventListener('click', event => {
   event.preventDefault()
   reloadWindow()
 })
 
+// function to set/reset the activity time based on user activity in window
+let inactiveTime = function () {
 
+  // create time variable and reset the timer when user moves mouse, keys or clicks
+  let time
+  window.onload = resetTimer
+  document.onmousemove = resetTimer
+  document.onkeydown = resetTimer
+  document.onclick = resetTimer
+
+  // function to log out and return to the login page
+  function logout() {
+    alert("User is logged out.")
+    localStorage.removeItem('token')
+    window.location = '/login.html'
+  }
+
+  // function to reset the timer (will log out after 20 min of no activity) 1000ms = 1s
+  function resetTimer() {
+    clearTimeout(time);
+    time = setTimeout(logout, 1200000)
+    // console.log(time)
+  }
+};
+
+// on window load call function of inactivity
+window.onload = function () {
+  inactiveTime();
+}
 
 // remove token after 30 min and go to log in page
 setTimeout(() => {
