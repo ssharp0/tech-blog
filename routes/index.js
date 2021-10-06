23f9d05a9bb694116fedbe94d80936dd3038d483
SHARP <ssharp9999@gmail.com>
2021-10-05 19:56:37 -0700

    prefix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
+// prefix api to the routes
 router.use('/api', require('./userRoutes.js'))
 router.use('/api', require('./postRoutes.js'))
+router.use('/api', require('./commentRoutes.js'))
 
-
+// export router
 module.exports = router
